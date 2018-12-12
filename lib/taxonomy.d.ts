14e8093f07147864f4830211d4e505ad0584a897
Suhail Abood <suhail.abood@gmail.com>
2018-12-12 00:27:24 +0000

    Adds term store

--- lib/taxonomy.d.ts
+++ lib/taxonomy.d.ts
@@ -46,3 +46,30 @@ export declare function termToJson(term: SP.Taxonomy.Term): {
     };
     CustomSortOrder: string;
 };
+export interface IValue {
+    id: string | number;
+    label: string;
+}
+export interface ITermStore {
+    createTerm(parentTerm: SP.Taxonomy.Term, name: string, locale: number, guid: SP.Guid, properties?: IValue[]): Promise<SP.Taxonomy.Term>;
+    getParentTermById(termId: string): Promise<SP.Taxonomy.Term>;
+    getTerms(termIds: string[]): Promise<SP.Taxonomy.Term[]>;
+    getTermsByTermId(termId: string): Promise<SP.Taxonomy.Term[]>;
+    getTermsByTermSetId(termSetId: string): Promise<SP.Taxonomy.Term[]>;
+    getAllTermsByTermSetId(termSetId: string): Promise<SP.Taxonomy.Term[]>;
+    termSetIdFromTaxonomyField(fieldInternalName: string): Promise<string>;
+    getTermsByIds(ids: string[]): Promise<SP.Taxonomy.TermCollection>;
+    getParentTermByTerm(term: SP.Taxonomy.Term): Promise<SP.Taxonomy.Term>;
+    getTopLevelParentOfTerm(id: string): Promise<SP.Taxonomy.Term>;
+    getTermParents(termId: string): Promise<SP.Taxonomy.Term[]>;
+    getParentThatSatisfies(id: string, fn: (v: SP.Taxonomy.Term) => boolean): Promise<SP.Taxonomy.Term>;
+    getTermLabelsById(termId: string): Promise<SP.Taxonomy.Label[]>;
+    getLabelsForTerms(terms: SP.Taxonomy.Term[]): Promise<SP.Taxonomy.LabelCollection[]>;
+    getTermsSubTreeFlat(termId: string, list?: SP.Taxonomy.Term[]): Promise<SP.Taxonomy.Term[]>;
+    getAllTermSetsInSiteCollectionGroup(createIfMissing: boolean): Promise<SP.Taxonomy.TermSet[]>;
+    getTermLabels(term: SP.Taxonomy.Term): Promise<SP.Taxonomy.Label[]>;
+    getTermById(id: string): Promise<SP.Taxonomy.Term>;
+    getSiteCollectionTermGroup(createIfMissing: any): Promise<SP.Taxonomy.TermGroup>;
+}
+export declare type TermStoreContextCallback<T> = (ctx: SP.ClientContext, session: SP.Taxonomy.TaxonomySession, tstore: SP.Taxonomy.TermStore, execute: (result: T) => void) => void;
+export declare function createTermStore(): ITermStore;
